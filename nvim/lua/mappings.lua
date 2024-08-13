c3b3e563a553e68e59ba8b30a0bd3d4a726d8cdd
yvoievid <yurii.voievidka@gmail.com>
2024-08-13 13:16:59 +0300

    debugging tool, testing tools for nvim for java

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,4 +11,51 @@ map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
 
+map('n', '<leader>gg', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+map('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+map('v', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+map('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<leader>gl', '<cmd>lua vim.diagnostic.open_float()<CR>')
+map('n', '<leader>gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+map('n', '<leader>gn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+map('n', '<leader>tr', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", '<leader>tc', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').test_class();
+  end
+end)
+
+map("n", '<leader>tm', function()
+  if vim.bo.filetype == 'java' then
+    require('jdtls').test_nearest_method();
+  end
+end)
+
+-- Debugging
+map("n", "<leader>Bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
+map("n", "<leader>Bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
+map("n", "<leader>Bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
+map("n", '<leader>Br', "<cmd>lua require'dap'.clear_breakpoints()<cr>")
+map("n", '<leader>Ba', '<cmd>Telescope dap list_breakpoints<cr>')
+map("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>")
+map("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
+map("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
+map("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
+map("n", '<leader>dd', function() require('dap').disconnect(); require('dapui').close(); end)
+map("n", '<leader>dt', function() require('dap').terminate(); require('dapui').close(); end)
+map("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
+map("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
+map("n", '<leader>di', function() require "dap.ui.widgets".hover() end)
+map("n", '<leader>d?', function() local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes) end)
+map("n", '<leader>df', '<cmd>Telescope dap frames<cr>')
+map("n", '<leader>dh', '<cmd>Telescope dap commands<cr>')
+map("n", '<leader>de', function() require('telescope.builtin').diagnostics({default_text=":E:"}) end)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -15,33 +15,50 @@ return {
       require "configs.lspconfig"
     end,
   },
- {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  {
+    'dccsillag/magma-nvim',
+    run = ':UpdateRemotePlugins'
   },
-}
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    init = function()
+      vim.g.molten_output_win_max_height = 12
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        'java-debug-adapter',
+        'java-test'
+  		},
+  	},
+  },
+  {
+    'mfussenegger/nvim-jdtls',
+    ft = 'java',
+  }
   --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
